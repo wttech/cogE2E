f78c78a7f8cb0d2e60e85d87b0b25ed67a5886d0
Adam Kosmala <adam.kosmala@cognifide.com>
2018-08-01 11:26:54 +0200

    Import fix

--- templates/puppeteer/jest-config/jest.config.common.js
+++ templates/puppeteer/jest-config/jest.config.common.js
@@ -1,14 +1,12 @@
 const path = require('path');
 
-module.exports = (args) => {
-    return {
-        globalSetup: './jest-config/setup.js',
-        globalTeardown: './jest-config/teardown.js',
-        testEnvironment: `./jest-config/puppeteer_environment.${args.env}.js`,
-        rootDir: path.join(__dirname, '..'),
-        testMatch: ['**/specs/**/*.js'],
-        transform: {
-            '^.+\\.js$': './jest-config/jestBabelTransform.js'
-        }
+module.exports = args => ({
+    globalSetup: './jest-config/setup.js',
+    globalTeardown: './jest-config/teardown.js',
+    testEnvironment: `./jest-config/puppeteer_environment.${args.env}.js`,
+    rootDir: path.join(__dirname, '..'),
+    testMatch: ['**/specs/**/*.js'],
+    transform: {
+        '^.+\\.js$': './jest-config/jestBabelTransform.js'
     }
-};
+});
