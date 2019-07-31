4568da8cd1e154038a30601a1dcfc90593ac55be
Adam Kosmala <adamkosmala@outlook.com>
2019-07-31 11:52:10 +0200

    Fix for case-sensitive systems (#8)

--- bin/setup.js
+++ bin/setup.js
@@ -1,24 +1,23 @@
-
-const inquirer = require('inquirer');
-const fs = require('fs-extra');
-const path = require('path');
-const merge = require('deepmerge')
-const shellExec = require('child_process').exec;
-const Spinner = require('cli-spinner').Spinner;
-const spinnerInstance = new Spinner('Installing dependencies...');
-const questions = require('./questions');
-const chalk = require('chalk');
+const inquirer = require("inquirer");
+const fs = require("fs-extra");
+const path = require("path");
+const merge = require("deepmerge");
+const shellExec = require("child_process").exec;
+const Spinner = require("cli-spinner").Spinner;
+const spinnerInstance = new Spinner("Installing dependencies...");
+const questions = require("./questions");
+const chalk = require("chalk");
 
 const paths = {
   outputPath: process.cwd(),
   templates: path.join(__dirname, "../templates")
 };
 
-const copyAssetsContent = async (tool) => {
+const copyAssetsContent = async tool => {
   const templates = path.join(__dirname, "../templates", tool);
 
   try {
-    console.log('Copying library assets...');
+    console.log("Copying library assets...");
     await fs.copy(templates, paths.outputPath);
   } catch (err) {
     console.error(err);
@@ -29,38 +28,57 @@ const updatePackage = async (projectPackage, toolPackage) => {
   try {
     const mergedPackage = await merge.all([projectPackage, toolPackage]);
 
-    await fs.writeFileSync(path.join(paths.outputPath, 'package.json'),JSON.stringify(mergedPackage, null, 2));
+    await fs.writeFileSync(
+      path.join(paths.outputPath, "package.json"),
+      JSON.stringify(mergedPackage, null, 2)
+    );
   } catch (err) {
     console.error(err);
   }
-}
+};
 
 inquirer.prompt(questions).then(answers => {
-  const {tool, packageAlready} = answers;
+  const { tool, packageAlready } = answers;
 
-  copyAssetsContent(tool);
+  const selectedTool = tool.toLowerCase();
+
+  copyAssetsContent(selectedTool);
 
   if (packageAlready) {
     try {
-      fs.readFileSync(path.join(paths.outputPath, 'package.json'));
-    } catch(err) {
-      console.log(chalk.red('Could not read package.json in project folder! Check if file exists'));
+      fs.readFileSync(path.join(paths.outputPath, "package.json"));
+    } catch (err) {
+      console.log(
+        chalk.red(
+          "Could not read package.json in project folder! Check if file exists"
+        )
+      );
       return;
     }
   }
 
-  const projectPackage = packageAlready ?
-    JSON.parse(fs.readFileSync(path.join(paths.outputPath, 'package.json'))) :
-    { "dependencies": {} };
+  const projectPackage = packageAlready
+    ? JSON.parse(fs.readFileSync(path.join(paths.outputPath, "package.json")))
+    : { dependencies: {} };
 
   try {
-    fs.readFileSync(path.join(paths.templates, `${tool}/packageTemplate.json`))
-  } catch(err) {
-    console.log(chalk.red('Could not read packageTemplate.json file in the ${tool} folder!'));
+    fs.readFileSync(
+      path.join(paths.templates, `${selectedTool}/package-template.json`)
+    );
+  } catch (err) {
+    console.log(
+      chalk.red(
+        "Could not read package-template.json file in the ${tool} folder!"
+      )
+    );
     return;
   }
 
-  const toolPackage = JSON.parse(fs.readFileSync(path.join(paths.templates, `${tool}/packageTemplate.json`)))
+  const toolPackage = JSON.parse(
+    fs.readFileSync(
+      path.join(paths.templates, `${selectedTool}/package-template.json`)
+    )
+  );
 
   updatePackage(projectPackage, toolPackage);
 
@@ -68,8 +86,8 @@ inquirer.prompt(questions).then(answers => {
   shellExec("npm install", (err, stdout) => {
     console.log(stdout);
 
-    fs.unlink(path.join(paths.outputPath, 'packageTemplate.json'));
-    console.log(chalk.green('Setup finished!'));
+    fs.unlink(path.join(paths.outputPath, "package-template.json"));
+    console.log(chalk.green("Setup finished!"));
   });
 
   spinnerInstance.stop();
